b6becc07e35be0e503e6f86cce69195cf0cc87a1
grafov <hrafov@gmail.com>
2021-09-20 20:32:51 +0300

    without comments at all

--- src/Main.java
+++ src/Main.java
@@ -5,47 +5,36 @@ public class Main {
     public static void main(String...args) throws IOException {
         File inputFile = new File("src/testLittle.in");
         Scanner in = new Scanner(inputFile);
-//        Scanner in = new Scanner(System.in);
         int t = in.nextInt() ; //number of test cases
-//        System.out.println("t = " + t);
         for (int testCase = 1; testCase <= t; testCase++) {
             int n = in.nextInt();
-            //System.out.println("n = " + n);
-            int[] array = new int[n];
-
+            long[] array = new long[n];
             boolean isZeros = false; // any element = 0
             for (int i = 0; i < n; i++) {
                 array[i] = in.nextInt();
-                if (array[i] == 0) {
-                    isZeros = true;
-                    break;
-                }
-                //System.out.println("   i = " + i + " " + array[i]);
+                if (array[i] == 0) isZeros = true;
             }
             if (isZeros) {
                 System.out.println("yes");
                 break;
             }
 
-            boolean isTrue = false;
+            boolean isSumEQZero = false;
             for (int firstElementNumber = 0; firstElementNumber < n - 1; firstElementNumber++) {
-
-                int sum = array[firstElementNumber];
-                //System.out.println(" sum = " + sum);
+                long sum = array[firstElementNumber];
                 for (int fENinSequence = firstElementNumber + 1; fENinSequence < n; fENinSequence++) {
                     sum += array[fENinSequence];
-                    //System.out.println("--- sum = " + sum);
                     if (sum == 0) {
-                        isTrue = true;
+                        isSumEQZero = true;
                         break;
                     }
                 }
-                if (isTrue) {
+                if (isSumEQZero) {
                     System.out.println("yes");
                     break;
                 }
             }
-            if (!isTrue) System.out.println("no");
+            if (!isSumEQZero) System.out.println("no");
         }
     }
 }
